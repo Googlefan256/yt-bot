58e760aea589b41fa4b7c09048624b47491cda40
Googlefan256 <yyykakeru@gmail.com>
2022-11-05 07:56:39 +0900

    removed dist

--- dist/lib/manager/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Voice = exports.VoiceManager = exports.Command = exports.CommandManager = void 0;
-var command_1 = require("./command");
-Object.defineProperty(exports, "CommandManager", { enumerable: true, get: function () { return command_1.CommandManager; } });
-Object.defineProperty(exports, "Command", { enumerable: true, get: function () { return command_1.Command; } });
-var voice_1 = require("./voice");
-Object.defineProperty(exports, "VoiceManager", { enumerable: true, get: function () { return voice_1.VoiceManager; } });
-Object.defineProperty(exports, "Voice", { enumerable: true, get: function () { return voice_1.Voice; } });
